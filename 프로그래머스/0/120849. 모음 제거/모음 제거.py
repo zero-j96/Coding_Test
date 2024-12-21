691e6f62680c53b1201c2b9f3b72cdcb64998c29
zero-j96 <jae_4942@naver.com>
2024-12-21 13:27:48 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    answer = my_string.replace('a','')
+    answer = answer.replace('e','')
+    answer = answer.replace('i','')
+    answer = answer.replace('o','')
+    answer = answer.replace('u','')
+    return answer
